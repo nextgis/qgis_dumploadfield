768d437e771f746817866ad3545991651036a999
GroveBow <ilja_02_2001@mail.ru>
2023-09-19 12:33:03 +0300

    add about

--- dumpfield.py
+++ dumpfield.py
@@ -26,43 +26,89 @@
 # ******************************************************************************
 
 # -*- coding: utf-8 -*-
+import os
+from os import path
+
 from qgis.PyQt.QtCore import *
 from qgis.PyQt.QtGui import *
 from qgis.core import *
 from qgis.PyQt.QtWidgets import *
+from qgis.PyQt.QtCore import QTranslator, QCoreApplication
+from qgis.core import QgsApplication
 
 from . import resources_rc
 from .dumpfield_dlgselfield import dlgSelField
+from . import about_dialog
 
 
 class dumpfield:
     def __init__(self, iface):
         """Initialize the class"""
         self.iface = iface
+        self.plugin_dir = path.dirname(__file__)
+        self._translator = None
+        self.__init_translator()
 
     def initGui(self):
-        self.actionToFile = QAction(QIcon(":/plugins/dumpfield/icon.png"), "Dump a field", self.iface.mainWindow())
+        self.actionToFile = QAction(QIcon(
+            ":/plugins/dumpfield/icon.png"), "Dump a field", self.iface.mainWindow()
+        )
         self.actionToFile.setStatusTip("Dump a field to a textfile")
         self.actionToFile.triggered.connect(self.dumpfield)
 
-        self.actionFromFile = QAction(QIcon(":/plugins/dumpfield/icon.png"), "Load to a field", self.iface.mainWindow())
+        self.actionFromFile = QAction(QIcon(
+            ":/plugins/dumpfield/icon.png"), "Load to a field", self.iface.mainWindow()
+        )
         self.actionFromFile.setStatusTip("Load text to a field from the textfile")
         self.actionFromFile.triggered.connect(self.loadtofield)
+
+        self.actionAbout = QAction(
+            self.tr("About"), self.iface.mainWindow()
+        )
+        self.actionAbout.triggered.connect(self.about)
 
         if hasattr(self.iface, "addPluginToVectorMenu"):
             self.iface.addPluginToVectorMenu("&Dump and load field", self.actionFromFile)
             self.iface.addPluginToVectorMenu("&Dump and load field", self.actionToFile)
+            self.iface.addPluginToVectorMenu("&Dump and load field", self.actionAbout)
         else:
             self.iface.addPluginToMenu("&Dump and load field", self.actionFromFile)
             self.iface.addPluginToMenu("&Dump and load field", self.actionToFile)
+            self.iface.addPluginToMenu("&Dump and load field", self.actionAbout)
 
     def unload(self):
         if hasattr(self.iface, "addPluginToVectorMenu"):
             self.iface.removePluginVectorMenu("&Dump and load field", self.actionToFile)
             self.iface.removePluginVectorMenu("&Dump and load field", self.actionFromFile)
+            self.iface.removePluginVectorMenu("&Dump and load field", self.actionAbout)
         else:
             self.iface.removePluginMenu("&Dump and load field", self.actionToFile)
             self.iface.removePluginMenu("&Dump and load field", self.actionFromFile)
+            self.iface.removePluginMenu("&Dump and load field", self.actionAbout)
+
+    def tr(self, message):
+        return QCoreApplication.translate(__class__.__name__, message)
+
+    def about(self):
+        dialog = about_dialog.AboutDialog(os.path.basename(self.plugin_dir))
+        dialog.exec_()
+
+    def __init_translator(self):
+        # initialize locale
+        locale = QgsApplication.instance().locale()
+
+        def add_translator(locale_path):
+            if not path.exists(locale_path):
+                return
+            translator = QTranslator()
+            translator.load(locale_path)
+            QCoreApplication.installTranslator(translator)
+            self._translator = translator  # Should be kept in memory
+
+        add_translator(path.join(
+            self.plugin_dir, 'i18n',
+            'dumploadfield_{}.qm'.format(locale)
+        ))
 
     def dumpfield(self):
         curLayer = self.iface.mapCanvas().currentLayer()
--- package.bat
+++ package.bat
@@ -1,9 +1,14 @@
 mkdir dumploadfield
+mkdir dumploadfield\i18n
 xcopy *.py dumploadfield
+xcopy *.ui dumploadfield
 xcopy README.md dumploadfield
 xcopy LICENSE dumploadfield
 xcopy metadata.txt dumploadfield
 xcopy icon.png dumploadfield
+xcopy i18n\dumploadfield_ru.ts dumploadfield\i18n\dumploadfield_ru.ts
+lrelease dumploadfield\i18n\dumploadfield_ru.ts
+del dumploadfield\i18n\dumploadfield_ru.ts
 zip -r dumploadfield.zip dumploadfield
-del /Q dumploadfield
-rd dumploadfield 
+del /s /q dumploadfield
+rmdir /s /q dumploadfield
